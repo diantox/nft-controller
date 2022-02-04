cfbf470e2dba20581c6161d78f5976bb99c19d08
Mira Absar <miras@miras.io>
2022-02-03 20:16:33 -0800

    Only connect to WLAN if wlan_ssid and wlan_password are set.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import config
 import hwrtc
 import wlan
 
 
 def main():
-    # wlan.initialize_wlan()
-    # wlan.connect_to_wlan()
+    if config.wlan_ssid and config.wlan_password:
+        wlan.initialize_wlan()
+        wlan.connect_to_wlan()
 
     hwrtc.initialize_hwrtc()
     hwrtc.initialize_ds3231()
