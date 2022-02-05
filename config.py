63247af738398b5af0b1eb0487a2c8cf415ec69f
Mira Absar <miras@miras.io>
2022-02-05 12:40:01 -0800

    Fix pump & LED enable pins.

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ i2c = I2C(scl=Pin(I2C_SCL), sda=Pin(I2C_SDA))
 ds3231_interrupt = Pin(15, Pin.IN, Pin.PULL_UP)
 ads1115_interrupt = Pin(27, Pin.IN, Pin.PULL_UP)
 
-pump_enable = Pin(14, Pin.OUT, Pin.PULL_UP, value=0)
-led_channel_0_enable = Pin(33, Pin.OUT, Pin.PULL_UP, value=0)
-led_channel_1_enable = Pin(32, Pin.OUT, Pin.PULL_UP, value=0)
+pump_enable = Pin(14, Pin.OUT, Pin.PULL_DOWN, value=0)
+led_channel_0_enable = Pin(33, Pin.OUT, Pin.PULL_DOWN, value=0)
+led_channel_1_enable = Pin(32, Pin.OUT, Pin.PULL_DOWN, value=0)
 
 pump_interrupt = Pin(26, Pin.IN, Pin.PULL_UP)
 led_interrupt = Pin(25, Pin.IN, Pin.PULL_UP)
